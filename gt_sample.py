f6860c7b8113e139eaab7fac825d7c369549bd69
NetBUG <oleg@skuuper.com>
2017-04-30 01:01:06 -0700

    Worked around buggy documentation for georasters

--- gt_sample.py
+++ gt_sample.py
@@ -3,15 +3,33 @@
 # @desc Fiddling around GeoTIFF
 
 import georasters as gr
+import numpy as np
+import matplotlib.pyplot as plt
 
+if __name__ == '__main__':
+    pass
 # Load data
-raster = './data/nasa-worldview-2016-07-09.tiff'
+#raster = './data/slope.tif' #nasa-worldview-2016-07-09.tiff'
+raster = './data/nasa-worldview-2016-07-20.tiff'
 data = gr.from_file(raster)
+NDV, xsize, ysize, GeoT, Projection, DataType = gr.get_geo_info(raster)
+
+print("NDV: %s; size: %dx%d; GeoT: %s; Projection: %s; Data: %s" % (NDV, xsize, ysize, GeoT, Projection, DataType))
+print()
+x = 101.45
+y = 66
+#col, row = gr.map_pixel(x,y,GeoT[1],GeoT[-1], GeoT[0],GeoT[3])
+#value = data[0,row,col]
+#print("%d, %d: %s" % (col, row, repr(value)))
 
 # Plot data
-print(data)
+#print(data)
+#plt.figure(figsize=(12, 8))
+data.raster = data.raster[0]
 data.plot()
+plt.show()
 
+'''
 # Get some stats
 data.mean()
 data.sum()
@@ -34,3 +52,4 @@ data3.plot()
 
 # Find value at point (x,y) or at vectors (X,Y)
 value = data.map_pixel(x,y)
+'''
